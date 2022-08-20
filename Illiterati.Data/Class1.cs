f4b2005b4aa470b855b4020990b43506b993a1a1
McKenzie Nalley <macnalley@gmail.com>
2022-08-20 12:25:21 -0400

    Created test project and data access layer project

---
+++ Illiterati.Data/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Illiterati.Data;
+public class Class1
+{
+
+}
---
+++ Illiterati.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Illiterati.Test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
